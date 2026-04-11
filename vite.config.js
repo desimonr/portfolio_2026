94d55138f7a4821a8e044528a27010f62ffabab4
Raymond <raymond0024@gmail.com>
2026-04-11 15:58:13 -0500

    Updated domain

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // if you're deploying to a GitHub Pages project repository,
-  // replace "repo-name" below with the actual repository name
-  base: '/portfolio_2026/',
+  // Custom domain – assets live at the root
+  base: '/',
   plugins: [react()],
 })
